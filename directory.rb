2c0d597b344625bd7001349ea912130facf9d4df
javier <js@silveriofunk.com>
2014-08-06 17:45:47 +0100

    Extracted a variable and added interpolation

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 24
 #first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "---------------"
@@ -26,7 +27,4 @@ puts "David Wickes"
 puts "Chris Oatley"
 puts "Marc Singh"
 #finally, we print the total
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 24
-puts " great students"
+puts "Overall, we have #{student_count} great students"
