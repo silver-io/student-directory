0a585c167885995e951f2f57af3e603491b083c7
javier <js@silveriofunk.com>
2014-08-07 12:31:57 +0100

    Refactored code to make it more readable

--- directory.rb
+++ directory.rb
@@ -1,29 +1,29 @@
 #let's put all students into an array
 students = [
-"Javier Silverio",
-"Elliot Lewis",
-"Ben Tillett",
-"Vincent Koch",
-"Michelle Ballard",
-"Nick Roberts",
-"Tatiana Soukiassian",
-"Mervé Silk",
-"Albert Vallverdu",
-"Lovis Schultze",
-"Henry Stanley",
-"Spike Lindsey",
-"Ruth Earle",
-"Andy Gates",
-"Faisal Aydarus",
-"Ethel Ng",
-"Kevin Daniells",
-"Maya Driver",
-"Leopold Kwok",
-"James McNeil",
-"Jerome Pratt",
-"David Wickes",
-"Chris Oatley",
-"Marc Singh",
+	{:name => "Javier Silverio", :cohort => :August},
+	{:name => "Elliot Lewis", :cohort => :August},
+	{:name => "Ben Tillett", :cohort => :August},
+	{:name => "Vincent Koch", :cohort => :August},
+	{:name => "Michelle Ballard", :cohort => :August},
+	{:name => "Nick Roberts", :cohort => :August},
+	{:name => "Tatiana Soukiassian", :cohort => :August},
+	{:name => "Mervé Silk", :cohort => :August},
+	{:name => "Albert Vallverdu", :cohort => :August},
+	{:name => "Lovis Schultze", :cohort => :August},
+	{:name => "Henry Stanley", :cohort => :August},
+	{:name => "Spike Lindsey", :cohort => :August},
+	{:name => "Ruth Earle", :cohort => :August},
+	{:name => "Andy Gates", :cohort => :August},
+	{:name => "Faisal Aydarus", :cohort => :August},
+	{:name => "Ethel Ng", :cohort => :August},
+	{:name => "Kevin Daniells", :cohort => :August},
+	{:name => "Maya Driver", :cohort => :August},
+	{:name => "Leopold Kwok", :cohort => :August},
+	{:name => "James McNeil", :cohort => :August},
+	{:name => "Jerome Pratt", :cohort => :August},
+	{:name => "David Wickes", :cohort => :August},
+	{:name => "Chris Oatley", :cohort => :August},
+	{:name => "Marc Singh", :cohort => :August}
 ]
 
 def print_header
@@ -31,9 +31,9 @@ def print_header
   puts "---------------"
 end
 
-def print(names)
-  names.each do |name|
-  	puts name
+def print(students)
+  students.each do |student|
+  	puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
