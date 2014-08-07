98b00b84aa8534d366a41e3a4f1b82df7d548544
javier <js@silveriofunk.com>
2014-08-07 13:04:39 +0100

    Getting the list of students from the user

--- directory.rb
+++ directory.rb
@@ -31,16 +31,29 @@ def print_header
   puts "---------------"
 end
 
-def print(students)
-  students.each do |student|
-  	puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  while !name.empty? do
+  	# add the student hash to the array
+  	students << {:name => name, :cohort => :november}
+  	puts "Now we have #{students.length} students"
+  	# get another name from the user
+  	name = gets.chomp
   end
+  # return the array of students
+  students
 end
 
 def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
